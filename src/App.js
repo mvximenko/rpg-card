e13b758fca2ebaec40e79875f3874e04543cc685
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2023-09-20 22:54:14 +0300

    Implement left side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
+import { useState } from 'react';
 import './styles/main.css';
 
+const fields = [
+  { tag: 'select', id: 'color', options: ['White', 'Green'] },
+  { id: 'name', placeholder: 'Sheoldred' },
+  { id: 'mana', title: 'Mana Cost', maxLength: 1, placeholder: '1' },
+  { id: 'image', type: 'file' },
+  { id: 'type', placeholder: 'Legendary Creature — Phyrexian Praector' },
+  { tag: 'textarea', id: 'description', placeholder: 'Deathtouch' },
+  {
+    id: 'power',
+    title: 'Power and Toughness',
+    maxLength: 3,
+    placeholder: '1/1',
+  },
+  { id: 'author', placeholder: 'mvximenko' },
+];
+
+const Field = ({ tag, id, title, options, ...props }) => {
+  const Component = tag || 'input';
+  return (
+    <div className='field'>
+      <label htmlFor={id}>{title ?? id}</label>
+      <Component id={id} {...props}>
+        {options &&
+          options.map((option, index) => <option key={index}>{option}</option>)}
+      </Component>
+    </div>
+  );
+};
+
 function App() {
-  return <div className='App'></div>;
+  const [state, setState] = useState({
+    color: '',
+    name: '',
+    mana: '',
+    image: '',
+    type: '',
+    description: '',
+    power: '',
+    author: '',
+  });
+
+  const handleChange = (event) => {
+    const { target } = event;
+
+    if (target.files?.[0]) {
+      setState((prevState) => ({
+        ...prevState,
+        [target.id]: URL.createObjectURL(target.files[0]),
+      }));
+    }
+
+    if (!target.files) {
+      setState((prevState) => ({
+        ...prevState,
+        [target.id]: target.value,
+      }));
+    }
+  };
+
+  return (
+    <main className='main'>
+      <div className='left'>
+        {fields.map((field) => (
+          <Field
+            key={field.id}
+            onChange={handleChange}
+            {...(field.type !== 'file' && { value: state[field.id] })}
+            {...field}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/styles.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
